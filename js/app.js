a736ac0cc8b8532583fda9eabde2ad7a1ee230ca
Draw-code7 <1571139878@qq.com>
2019-09-06 17:30:06 +0800

    Site updated: 2019-09-06 17:30:02

--- js/app.js
+++ js/app.js
@@ -48,3 +48,23 @@ $("article:not(.nexmoe-py) img").each(function() {
 
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
 mdui.mutation();
+
+window.onload = function() {
+  var OriginTitile = document.title;
+  var titleTime;
+  document.addEventListener('visibilitychange', function() {
+    if(document.hidden) {
+      $('[rel="icon"]').attr('href', "/bitbug_favicon.ico");
+      $('[rel="shortcut icon"]').attr('href', "/bitbug_favicon.ico");
+      document.title = '喔唷，崩溃啦！';
+      clearTimeout(titleTime);
+    } else {
+      $('[rel="icon"]').attr('href', "/bitbug_favicon.ico");
+      $('[rel="shortcut icon"]').attr('href', "/bitbug_favicon.ico");
+      document.title = '咦，页面又好了！';
+      titleTime = setTimeout(function() {
+        document.title = OriginTitile;
+      }, 2000);
+    }
+  });
+}
