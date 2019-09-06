07c844fb03369d31b876423a2f8782dc97bc2029
Draw-code7 <1571139878@qq.com>
2019-09-06 17:00:52 +0800

    Site updated: 2019-09-06 17:00:49

--- js/src/custom.js
+++ js/src/custom.js
@@ -4,13 +4,13 @@ window.onload = function() {
   var titleTime;
   document.addEventListener('visibilitychange', function() {
     if(document.hidden) {
-      $('[rel="icon"]').attr('href', "/themes/nexmoe/source//images/bitbug_favicon.ico");
-      $('[rel="shortcut icon"]').attr('href', "/themes/nexmoe/source//images/bitbug_favicon.ico");
+      $('[rel="icon"]').attr('href', "/bitbug_favicon.ico");
+      $('[rel="shortcut icon"]').attr('href', "/bitbug_favicon.ico");
       document.title = '喔唷，崩溃啦！';
       clearTimeout(titleTime);
     } else {
-      $('[rel="icon"]').attr('href', "/themes/nexmoe/source//images/bitbug_favicon.ico");
-      $('[rel="shortcut icon"]').attr('href', "/themes/nexmoe/source//images/bitbug_favicon.ico");
+      $('[rel="icon"]').attr('href', "/bitbug_favicon.ico");
+      $('[rel="shortcut icon"]').attr('href', "/bitbug_favicon.ico");
       document.title = '咦，页面又好了！';
       titleTime = setTimeout(function() {
         document.title = OriginTitile;
